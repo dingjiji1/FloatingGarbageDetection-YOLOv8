7bffca0891d03d7dac2b5d2d18883d9d884ca409
dingjiji1 <1872296356@qq.com>
2025-06-12 12:25:04 +0800

    “你的提交信息”

---
+++ train.py
@@ -0,0 +1,20 @@
+
+
+import os
+import torch
+from ultralytics import YOLO
+# device = torch.device('gpu')
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
+
+import torch
+print(torch.__version__)
+
+if __name__ == '__main__':
+    model = YOLO('./ultralytics/cfg/models/v8/yolov8-C2f-ScConv.yaml')
+    # result = model.train(data = 'data2.yaml', epochs=100, imgsz=416,batch = 4)
+
+
+
+
+    result = model.train(data = 'data3.yaml', epochs=100, imgsz=640,batch=4)
